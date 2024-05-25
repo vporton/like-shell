6cb0d0fcc35e8e68b19d036d5597c6cb932ff79f
Victor Porton <porton.victor@gmail.com>
2024-05-25 09:52:49 +0300

    doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::env::set_current_dir;
 use fs_extra::dir::CopyOptions;
 use tempdir::TempDir;
 
+/// Create a temporary directory filled with a copy of `source_dir`.
 pub fn temp_dir_from_template(source_dir: &str) -> Result<TempDir, Box<dyn std::error::Error>> {
     let temp_dir = TempDir::new("test")?;
     let options = CopyOptions::new();
