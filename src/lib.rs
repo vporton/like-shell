2206d8d615c021994d40e51b468a37ad50acefd3
Victor Porton <porton.victor@gmail.com>
2024-05-25 09:47:05 +0300

    function renamed

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::env::set_current_dir;
 use fs_extra::dir::CopyOptions;
 use tempdir::TempDir;
 
-pub fn dir_with_content(source_dir: &str) -> Result<TempDir, Box<dyn std::error::Error>> {
+pub fn temp_dir_from_template(source_dir: &str) -> Result<TempDir, Box<dyn std::error::Error>> {
     let temp_dir = TempDir::new("test")?;
     let options = CopyOptions::new();
     fs_extra::dir::copy(source_dir, temp_dir.path(), &options)?;
