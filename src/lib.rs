25065f765a8e0c707530fea2bae0d8518564f46d
Victor Porton <porton.victor@gmail.com>
2024-05-27 22:12:19 +0300

    alleged bug fix

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use tempdir::TempDir;
 /// Create a temporary directory filled with a copy of `source_dir`.
 pub fn temp_dir_from_template(source_dir: &Path) -> Result<TempDir, Box<dyn std::error::Error>> {
     let temp_dir = TempDir::new("test")?;
-    let options = CopyOptions::new();
+    let options = CopyOptions::new().copy_inside(true);
     fs_extra::dir::copy(source_dir, temp_dir.path(), &options)?;
     set_current_dir(temp_dir.path())?;
     Ok(temp_dir)
